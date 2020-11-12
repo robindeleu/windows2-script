765affb2676baad01a77ba6dad4f7d955325c25a
Robin Deleu <deleu.robin@outlook.com>
2020-11-12 08:00:29 +0100

    vergrlijken weerkt !! :)

--- windowsscript.ps1
+++ windowsscript.ps1
@@ -70,14 +70,15 @@ function Installed-Programs {
     #DIT DEEL WERKT NOG NIET
     # voor het opslaan van dit bestand:
     #$storelocation= D:\Users\deleu\Documents\VIVES\Fase 3\Windows2\windows2-script\script-befor.txt
-    Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize > D:\Users\deleu\Documents\VIVES\Fase 3\Windows2\windows2-script\script-befor.txt
+    Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize > D:\Users\deleu\Documents\VIVES\powershell.txt
+
 
     #Als je wil vergelijken voor en na
     
     #$befor = C:\Users\deleu\Documents\InstalledPrograms-PS-befor.txt
     #$after = C:\Users\deleu\Documents\InstalledPrograms-PS-after.txt
     
-    #Compare-Object -ReferenceObject (Get-Content $befor) -DifferenceObject (Get-Content $after)
+    #Compare-Object -ReferenceObject (Get-Content C:\Users\deleu\Documents\InstalledPrograms-PS-befor.txt) -DifferenceObject (Get-Content C:\Users\deleu\Documents\InstalledPrograms-PS-after.txt)
 
 }
 
