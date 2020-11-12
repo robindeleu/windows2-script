b0aeaf2b1c60af0c94a770471f4d0d1fb0734089
Robin Deleu <deleu.robin@outlook.com>
2020-11-12 09:20:15 +0100

    shares af

---
+++ shares.ps1
@@ -0,0 +1,12 @@
+function Shares {
+    Write-Host "`n------------------------------------------------------" -ForegroundColor Green
+    Write-Host "                Checking 'All shares'"
+    Write-Host "------------------------------------------------------`n" -ForegroundColor Green
+    $checkshares = Get-SmbShare | Select-Object Name, Path, Description| Format-Table -AutoSize
+    $checkshares
+    #Dit is om op te slaan van alle shares op dit moment in een text file
+    #$checkshares > D:\Users\deleu\Documents\VIVES\powershell1.txt
+    #Dit is om te vergelijken tussen oud en nieuw
+    #Compare-Object -ReferenceObject (Get-Content D:\Users\deleu\Documents\VIVES\powershell.txt) -DifferenceObject (Get-Content D:\Users\deleu\Documents\VIVES\powershell1.txt)
+}
+Shares
