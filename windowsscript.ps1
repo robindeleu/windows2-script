0274b1bb92d3386bf0cbf71406c558da032fe479
Robin Deleu <deleu.robin@outlook.com>
2020-11-12 07:56:40 +0100

    update om te vergelijken tussen programs werkt nog niet

--- windowsscript.ps1
+++ windowsscript.ps1
@@ -67,9 +67,10 @@ function Installed-Programs {
     Write-Host "------------------------------------------------------`n" -ForegroundColor Green
     Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize
     
+    #DIT DEEL WERKT NOG NIET
     # voor het opslaan van dit bestand:
-    #$storelocation= C:\Users\deleu\Documents\InstalledPrograms-PS-befor.txt
-    #Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize > $storelocation
+    #$storelocation= D:\Users\deleu\Documents\VIVES\Fase 3\Windows2\windows2-script\script-befor.txt
+    Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize > D:\Users\deleu\Documents\VIVES\Fase 3\Windows2\windows2-script\script-befor.txt
 
     #Als je wil vergelijken voor en na
     
