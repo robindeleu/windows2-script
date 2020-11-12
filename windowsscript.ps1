8157bb39d74bb2e31682d71678402824d2ad69b5
Robin Deleu <deleu.robin@outlook.com>
2020-11-12 08:53:21 +0100

    vergrlijken weerkt !! :)

--- windowsscript.ps1
+++ windowsscript.ps1
@@ -68,7 +68,7 @@ function Installed-Programs {
     Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize
     
     #DIT DEEL WERKT NOG NIET
-    # voor het opslaan van dit bestand:
+    #voor het opslaan van dit bestand:
     #$storelocation= D:\Users\deleu\Documents\VIVES\Fase 3\Windows2\windows2-script\script-befor.txt
     Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize > D:\Users\deleu\Documents\VIVES\powershell.txt
 
