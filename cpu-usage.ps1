c228532cd4449017a05e63c44719a0c17817d5e9
Robin Deleu <deleu.robin@outlook.com>
2020-11-12 09:37:49 +0100

    toevoegen van top 10 processes

--- cpu-usage.ps1
+++ cpu-usage.ps1
@@ -4,4 +4,7 @@ function CPU-Usage {
     Write-Host "------------------------------------------------------`n" -ForegroundColor Green
     $Processor = (Get-WmiObject -Class win32_processor| Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average
     Write-Output "$Processor%"
+    $processescpu = Get-Counter -ErrorAction SilentlyContinue '\Process(*)\% Processor Time' | Select-Object -ExpandProperty countersamples| Select-Object -Property instancename, cookedvalue| ? {$_.instanceName -notmatch "^(idle|_total|system)$"} | Sort-Object -Property cookedvalue -Descending| Select-Object -First 10| ft InstanceName,@{L='CPU';E={($_.Cookedvalue/100/$env:NUMBER_OF_PROCESSORS).toString('P')}} -AutoSize
+    $processescpu
 }
+CPU-Usage
