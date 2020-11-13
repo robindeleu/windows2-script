a7e57553bbbb6f9d6da5c066fe15bb94b524ae0d
Maxim Vanden Abeele <maxim.vandenabeele@gmail.com>
2020-11-13 10:14:05 +0100

    change event iteration to eventlist not eventid list and format output to table

--- log-event-ids/application-event-ids.ps1
+++ log-event-ids/application-event-ids.ps1
@@ -1,19 +1,5 @@
 $applicationEventIds = @{
 
-    0 = "nul"
-    13 = "dertien"
-    63 = "drieenzestig"
-    100 = "Het systeem kan de opgegeven omgevingsoptie niet vinden"
-    455 = "vierhonderenvijfenvijftig"
-    902 = "negenhonderdentwee"
-    903 = "negenhonderdendrie"
-    1008 = "duizendenacht"
-    1013 = "duizenddertien"
-    1020 = "duizendtwintig"
-    1026 = "duizendzesentwintig"
-    2002 = "tweeduizendentwee"
-    2003 = "tweeduizendendrie"
-    6004 = "zesduizendenvier"
-    8193 = "achtduizendhonderd drieennegentig"
+    100 = "Het systeem kan de opgegeven omgevingsoptie niet vinden."
 
 }
--- show-event-logs.ps1
+++ show-event-logs.ps1
@@ -14,67 +14,71 @@ function Show-Event-Logs {
     # GROUP ALL SECURITY LOG ENTRIES BY EVENT ID
     # AND PRINT OUT THE AMOUNTS FOR EVENT IDs OF INTEREST
     $securityEventsSummary = Get-Eventlog -Logname Security -After $Begin | where {($_.EntryType -NotMatch "Information")}  | Group-Object -property EventID | Sort-Object -Property Count -Descending
-    foreach ($event in $securityEventIds.keys) {
-        $amount = ($securityEventsSummary | where {($_.Name -eq "$event")} | Select-Object -ExpandProperty count)
-        if ($amount -gt 0) {
-            Write-Host $securityEventIds[$event] ": " $amount
-        }
+    $securityEventList = @()
+
+    foreach ($event in $securityEventsSummary) {
+        $amount = ($event | Select-Object -ExpandProperty Count)
+        $name = ($event | Select-Object -ExpandProperty Name)
+        $eventId = [int]::Parse($name)
+        $info = if ($securityEventIds[$eventId]) {$securityEventIds[$eventId]} else {"No description"}
+        $securityEventList += [pscustomobject]@{EventId = $eventId; Info = $info; Count = $amount}
     }
 
+    $securityEventList | Format-Table EventId, Count, Info
+
     # Leave a blank line after the last output
     Write-Host "`n"
 
 
 
-    Write-Host "`n------------------------------------------------------" -ForegroundColor Green
+    Write-Host "`n------------------------------------------------------" -ForegroundColor Yellow
     Write-Host "                 CHECKING SYSTEM LOG                    "
-    Write-Host "------------------------------------------------------`n" -ForegroundColor Green
-    # Write-Host "`n========> System-log: All non-information Entries`n" -ForegroundColor Yellow
-    # $systemEvents = Get-Eventlog -logname System -After $Begin | where {($_.EntryType -NotMatch "Information")} | Sort-Object -Property EventId | Format-Table EventId, Timegenerated, EntryType, Source, Message -autosize
-    # Write-Host ($systemEvents | Out-String)
+    Write-Host "------------------------------------------------------`n" -ForegroundColor Yellow
 
+    # Import hashtable of known system event id's
+    . .\log-event-ids\system-event-ids.ps1
+
+    # GROUP ALL SECURITY LOG ENTRIES BY EVENT ID
+    # AND PRINT OUT THE AMOUNTS FOR EVENT IDs OF INTEREST
     $systemEventsSummary = Get-Eventlog -Logname System -After $Begin | where {($_.EntryType -NotMatch "Information")}  | Group-Object -property EventID | Sort-Object -Property Count -Descending
-    Write-Host ($systemEventsSummary | Out-String)
+    $systemEventList = @()
 
-    #   # Import hashtable of known system event id's
-    #   . .\log-event-ids\system-event-ids.ps1
+    foreach ($event in $systemEventsSummary) {
+        $amount = ($event | Select-Object -ExpandProperty Count)
+        $name = ($event | Select-Object -ExpandProperty Name)
+        $eventId = [int]::Parse($name)
+        $info = if ($systemEventIds[$eventId]) {$systemEventIds[$eventId]} else {"No description"}
+        $systemEventList += [pscustomobject]@{EventId = $eventId; Info = $info; Count = $amount}
+    }
 
-    # # GROUP ALL SECURITY LOG ENTRIES BY EVENT ID
-    # # AND PRINT OUT THE AMOUNTS FOR EVENT IDs OF INTEREST
-    # $sytemEventsSummary = Get-Eventlog -Logname System -After $Begin | where {($_.EntryType -NotMatch "Information")}  | Group-Object -property EventID | Sort-Object -Property Count -Descending
-    # foreach ($event in $systemEventIds.keys) {
-    #     $amount = ($applicationEventsSummary | where {($_.Name -eq "$event")} | Select-Object -ExpandProperty count)
-    #     if ($amount -gt 0) {
-    #         Write-Host $systemEventIds[$event] ": " $amount
-    #     }
-    # }
+    $systemEventList | Format-Table EventId, Count, Info
 
     # Leave a blank line after the last output
     Write-Host "`n"
 
 
-    Write-Host "`n------------------------------------------------------" -ForegroundColor Green
-    Write-Host "             Checking Application Log                   "
-    Write-Host "------------------------------------------------------`n" -ForegroundColor Green
-    # Write-Host "`n========> Application-log: All non-information Entries`n" -ForegroundColor Yellow
-    # $applicationEvents = Get-Eventlog -Logname Application -After $Begin | where {($_.EntryType -NotMatch "Information")} | Sort-Object -Property EventId | Format-Table EventId, Timegenerated, EntryType, Source, Message -autosize
-    # Write-Host ($applicationEvents | Out-String)
 
-    # $applicationEventsSummary = Get-Eventlog -Logname Application -After $Begin | where {($_.EntryType -NotMatch "Information")}  | Group-Object -property EventID | Sort-Object -Property Count -Descending
-    # Write-Host ($applicationEventsSummary | Out-String)
+    Write-Host "`n------------------------------------------------------" -ForegroundColor Yellow
+    Write-Host "             Checking Application Log                   "
+    Write-Host "------------------------------------------------------`n" -ForegroundColor Yellow
 
     # Import hashtable of known application event id's
     . .\log-event-ids\application-event-ids.ps1
 
-    # GROUP ALL SECURITY LOG ENTRIES BY EVENT ID
+    # GROUP ALL APPLICATION LOG ENTRIES BY EVENT ID
     # AND PRINT OUT THE AMOUNTS FOR EVENT IDs OF INTEREST
     $applicationEventsSummary = Get-Eventlog -Logname Application -After $Begin | where {($_.EntryType -NotMatch "Information")}  | Group-Object -property EventID | Sort-Object -Property Count -Descending
-    foreach ($event in $applicationEventIds.keys) {
-        $amount = ($applicationEventsSummary | where {($_.Name -eq "$event")} | Select-Object -ExpandProperty count)
-        if ($amount -gt 0) {
-            Write-Host $applicationEventIds[$event] ": " $amount
-        }
+    $applicationEventList = @()
+
+    foreach ($event in $applicationEventsSummary) {
+        $amount = ($event | Select-Object -ExpandProperty Count)
+        $name = ($event | Select-Object -ExpandProperty Name)
+        $eventId = [int]::Parse($name)
+        $info = if ($applicationEventIds[$eventId]) {$applicationEventIds[$eventId]} else {"No description"}
+        $applicationEventList += [pscustomobject]@{EventId = $eventId; Info = $info; Count = $amount}
     }
+    
+    $applicationEventList | Format-Table EventId, Count, Info
 
     # Leave a blank line after the last output
     Write-Host "`n"
