d5479ea5bc62cd75cdaa055c4c77be2d503f27c7
Benjamin Dhondt <benjamin.dhondt@hotmail.com>
2020-11-17 09:54:34 +0100

    added backup for output

--- users.ps1
+++ users.ps1
@@ -19,17 +19,17 @@ function Users {
     Write-Host "Number of Local accounts: " $cntLocals
     Write-Host "Number of Microsoft accounts: " $cntMCAccounts "`n"
 
-    $objects = @{
-    ReferenceObject = (Get-Content -Path ../usersbefore.txt)
-    DifferenceObject = (Get-Content -Path ../usersafter.txt)
+    Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > ../usersafter.txt
+    Compare-Object -ReferenceObject (Get-Content ../usersbefore.txt) -DifferenceObject (Get-Content ../usersafter.txt)
+    if(Compare-Object -ReferenceObject (Get-Content ../usersbefore.txt) -DifferenceObject (Get-Content ../usersafter.txt)) {
+        Write-Host "Files are different"
+    }
+    else {
+        Write-Host  "Files are the same"
     }
 
-#Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > ../usersafter.txt
-Compare-Object -ReferenceObject (Get-Content ../usersbefore.txt) -DifferenceObject (Get-Content ../usersafter.txt)
-if(Compare-Object -ReferenceObject (Get-Content ../usersbefore.txt) -DifferenceObject (Get-Content ../usersafter.txt)) {
-    Write-Host "Files are different"
-}
-else {
-    Write-Host  "Files are the same"
-}
+    $From = Get-Content -Path ../usersbefore.txt
+    Add-Content -Path ../backupUsers.txt -Value $From
+    
+    Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > ../usersbefore.txt
 }
