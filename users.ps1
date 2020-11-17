7b9a9461ae58b5ab62f9e15ba36b58ef18b3f252
Benjamin Dhondt <benjamin.dhondt@hotmail.com>
2020-11-17 10:55:54 +0100

    added date

--- users.ps1
+++ users.ps1
@@ -3,7 +3,12 @@ function Users {
     Write-Host "                Checking Users                           "
     Write-Host "------------------------------------------------------`n" -ForegroundColor Green
 
+    $fileBefore = "../usersafter.txt"
+    $fileAfter = "../usersbefore.txt"
+    $fileBackup = "../backupUsers.txt"
+
     Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description | Format-Table -AutoSize
+    Get-Date -Format "dddd MM/dd/yyyy HH:mm K`n"
 
     $Locals = Get-LocalUser | Where-Object PrincipalSource -eq "Local" | Select PrincipalSource
     $mcAccounts = Get-LocalUser | Where-Object PrincipalSource -eq "MicrosoftAccount" | Select PrincipalSource
@@ -19,17 +24,19 @@ function Users {
     Write-Host "Number of Local accounts: " $cntLocals
     Write-Host "Number of Microsoft accounts: " $cntMCAccounts "`n"
 
-    Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > ../usersafter.txt
-    Compare-Object -ReferenceObject (Get-Content ../usersbefore.txt) -DifferenceObject (Get-Content ../usersafter.txt)
-    if(Compare-Object -ReferenceObject (Get-Content ../usersbefore.txt) -DifferenceObject (Get-Content ../usersafter.txt)) {
+    Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > $fileAfter
+    Compare-Object -ReferenceObject (Get-Content $fileBefore) -DifferenceObject (Get-Content $fileAfter)
+    if(Compare-Object -ReferenceObject (Get-Content $fileBefore) -DifferenceObject (Get-Content $fileAfter)) {
         Write-Host "Files are different"
     }
     else {
         Write-Host  "Files are the same"
     }
 
-    $From = Get-Content -Path ../usersbefore.txt
-    Add-Content -Path ../backupUsers.txt -Value $From
+    $Date = Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
+    $From = Get-Content -Path $fileBefore
+    Add-Content -Path $fileBackup -Value $From
+    Add-Content -Path $fileBackup -Value $Date
     
-    Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > ../usersbefore.txt
+    Get-LocalUser | Sort-Object PrincipalSource | Select Name, PrincipalSource, LastLogon, PasswordChangeableDate, PasswordLastSet, Description > $fileBefore
 }
