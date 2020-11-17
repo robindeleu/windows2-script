cadb55fceb7c4ae01531a9febaf9ed1f90195366
Robin Deleu <deleu.robin@outlook.com>
2020-11-17 11:37:44 +0100

    aanpassingen in processes

--- live-processes.ps1
+++ live-processes.ps1
@@ -2,6 +2,9 @@ function Live-Processes {
     Write-Host "`n------------------------------------------------------" -ForegroundColor Green
     Write-Host "                Checking 'All live processes working'"
     Write-Host "------------------------------------------------------`n" -ForegroundColor Green
-    $A = Get-Process
-    $A | Get-Process | Format-Table -View priority
+
+    # This is just a simple CMDLET that makes that we can print out the processes and see the priority
+    # $A = Get-Process
+    # $A | Get-Process | Format-Table -View priority
+    Get-Process | Sort-Object -Property cpu -Descending | Select-Object -First 10
 }
